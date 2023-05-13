7b2d46a7284188a5d0fd39c02fa263569c73c668
yurim3 <w2103@e-mirim.hs.kr>
2023-05-14 00:09:05 +0900

    페이지 왼쪽 탭 프로필 코드(이미지, 이름, sns 로고, 한줄소개 삽입)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
-import myPortFolio from "img/myPortFolio.jpg";
+import myProfileImg from "./img/myProfileImg.jpg";
+import myGithub from "./img/githubLinkLogo.png";
+import myGmail from "./img/gmailLinkLogo.png";
+import myInsta from "./img/instaLinkLogo.png";
 
 function LeftSection() {
   return (
     <div
       className="left-section"
-      style={{ backgroundColor: "#081B4B", width: "400px" }}
+      style={{backgroundColor: "#1f416d", width: "400px"}}
     >
-      <img src={myPortFolio} alt="My Portfolio" />
-      {/* ... */}
+    <img src={myProfileImg} alt="My ProfileImg" className="myProfileImg"/>
+      <p className="name">신유림</p>
+      <div className="SNSLink">
+        <img src={myGithub} alt="My Github" className="myGithub"/>
+        <img src={myGmail} alt="My Gmail" className="myGmail"/>
+        <img src={myInsta} alt="My Insta" className="myInsta"/>
+        {/* ... */}
+      </div>
+      <p className="myMessage">&nbsp;숲이 자연의 재능을 가진 나무들로 이루어져 있듯이, 하나의 팀으로 놀라운 결과물을 만들어내는 개발자가 되고 싶습니다.</p>
     </div>
   );
 }
